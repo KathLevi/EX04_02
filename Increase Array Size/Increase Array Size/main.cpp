f645eb8d60c660f56e0340c2432c83f88c436ef1
KathLevi <kathleenlevi@Kathleens-MacBook-Pro.local>
2015-06-16 21:38:56 -0700

    updated
    I think I did it correctly, by just inputting the old array into the
    new one :)

--- Increase Array Size/Increase Array Size/main.cpp
+++ Increase Array Size/Increase Array Size/main.cpp
@@ -6,12 +6,12 @@
 //Date: May 26, 2015
 //////////////////////////
 
-
 #include <iostream>
 using namespace std;
-
-int* doubleCapacity (const int* list, int size);
-void addNumbers (int* list, int size);
+template <typename T>
+T* doubleCapacity (const T* list, T size);
+template <typename T>
+void addNumbers (T* list, T size);
 
 int main() {
     
@@ -26,8 +26,8 @@ int main() {
     
     delete []list;
 }
-
-void addNumbers (int* list, int size){
+template <typename T>
+void addNumbers (T* list, T size){
     for (int i = 0; i < size; i++){
         cout << "Enter the next number in the array ";
         cin >> list[i];
@@ -35,10 +35,13 @@ void addNumbers (int* list, int size){
     
 }
 
-int* doubleCapacity (const int* list, int size){
+template <typename T>
+T* doubleCapacity (const T* list, T size){
     size *= 2;
     int* doubleList = new int[size];
-    
+    for (int i = 0; i < size; i ++){
+        doubleList[i] = list[i];
+    }
     return doubleList;
 }
 //it doesnt say in the book that we have to read numbers into the new array or output anything, if we do let me know and I can add that in.
